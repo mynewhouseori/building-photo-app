3a2036c5103ccc3c04fb6005e541182c070ffca6
ori levin <mynewhouseori@gmail.com>
2026-05-18 14:01:06 +0300

    Enable Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
 window.BUILDING_PHOTO_FIREBASE = {
-  enabled: false,
+  enabled: true,
   trialDays: 7,
   config: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: ""
+    apiKey: "AIzaSyBmK011s1fYmLoguxwWB6FBk5sO8xbFKqs",
+    authDomain: "building-photo-app.firebaseapp.com",
+    projectId: "building-photo-app",
+    storageBucket: "building-photo-app.firebasestorage.app",
+    messagingSenderId: "132231591417",
+    appId: "1:132231591417:web:ddf6d41470183801bdb928",
+    measurementId: "G-KTG0P2GDWF"
   }
 };
